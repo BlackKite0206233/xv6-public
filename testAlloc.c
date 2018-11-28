05dfba4f820e2b9f13067697a9b5c5b871dac6c5
blackkite0206233 <samuel991604@gmail.com>
2018-11-28 14:17:48 +0800

    add test

---
+++ testAlloc.c
@@ -0,0 +1,29 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+typedef struct test {
+    int a;
+    unsigned long long b;
+    char c;
+} Test;
+
+int main() {
+    Test t;
+    t.a = 1;
+    t.b = -1;
+    t.c = 'Z';
+
+    int len = 10;
+    Test *tArray = (Test *)valloc(len, sizeof(Test), &t);
+    for (int i = 0; i < len; i++) {
+        printf(1, "%d\t%#012x\t", i, tArray + i);
+        Test *t_i = &tArray[i];
+        char *c = (char *)t_i;
+        for (int j = 0; j < sizeof(Test); j++) {
+            printf(1, "%02X ", *(c + j));
+        }
+        printf(1, "\n");
+    }
+}
