82d00cd11b5a6f5a8b6428883e16bf12c373f084
blackkite0206233 <samuel991604@gmail.com>
2018-11-28 14:32:09 +0800

    printf

--- testAlloc.c
+++ testAlloc.c
@@ -18,7 +18,7 @@ int main() {
     int len = 10;
     Test *tArray = (Test *)valloc(len, sizeof(Test), &t);
     for (int i = 0; i < len; i++) {
-        printf(1, "%d\t%#012x\t", i, tArray + i);
+        printf(1, "%d\t%#012X\t", i, tArray + i);
         Test *t_i = &tArray[i];
         char *c = (char *)t_i;
         for (int j = 0; j < sizeof(Test); j++) {
