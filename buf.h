d82260745f5754701cc4a7de34f762d97559d758
blackkite0206233 <samuel991640@gmail.com>
2018-12-23 14:03:23 +0800

    Fix some bugs

--- buf.h
+++ buf.h
@@ -1,3 +1,6 @@
+#include "sleeplock.h"
+#include "fs.h"
+
 struct buf {
   int flags;
   uint dev;
@@ -9,6 +12,7 @@ struct buf {
   struct buf *qnext; // disk queue
   uchar data[BSIZE];
 };
+#define B_BUSY  0x1  // buffer is locked by some process
 #define B_VALID 0x2  // buffer has been read from disk
 #define B_DIRTY 0x4  // buffer needs to be written to disk
 
